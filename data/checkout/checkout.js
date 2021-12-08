ed393b6829e8464bc19481e6c94eab527d9e59bc
harish b <harish.balasubramanian15@siesgst.ac.in>
2021-12-08 03:09:09 -0500

    Checkout layout completed

---
+++ data/checkout/checkout.js
@@ -0,0 +1,15 @@
+const showPayDetails=(req,res)=>{
+    res.render('pages/checkout/orderSummary');
+}
+
+const bookTicket=()=>{
+
+}
+
+const showTicket=()=>{
+
+}
+
+module.exports={
+    showPayDetails,bookTicket
+}
