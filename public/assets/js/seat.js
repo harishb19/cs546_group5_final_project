933cbd1d41fee2261c5a2cdc9618a3707f5dfa0b
harish b <harish.balasubramanian15@siesgst.ac.in>
2021-12-11 22:00:55 -0500

    code reformat

--- public/assets/js/seat.js
+++ public/assets/js/seat.js
@@ -76,7 +76,7 @@ continueButton.addEventListener('click', e => {
         theatreId: theatreId,
         runtime: runtime,
         language: language,
-        screenId:screenId,
+        screenId: screenId,
         theatreName: theatreName,
         dateTime: movieDate,
         noOfSeats: seatList.length,
